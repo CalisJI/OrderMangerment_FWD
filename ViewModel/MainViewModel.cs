0cd2eebc9ad7bddab7e58ef9b179683aea7d2307
CalisJI <quythi.buinguyen@gmail.com>
2022-04-04 16:08:58 +0700

    Brand 2 commit

---
+++ ViewModel/MainViewModel.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrderManagerment_WPF.ViewModel
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private BaseViewModel _selectedview;
+        public BaseViewModel SelectedViewModel
+        {
+            get 
+            {
+                return _selectedview;
+            }
+            set 
+            {
+                SetProperty(ref _selectedview, value, nameof(SelectedViewModel));
+            }
+        }
+        public MainViewModel mainViewModel;
+        public MainViewModel() 
+        {
+            mainViewModel = this;
+        }
+    }
+}
