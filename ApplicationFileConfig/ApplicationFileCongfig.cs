a94dfe797a2484a7779c3cabd5d8e6e4ea73139d
CalisJI <quythi.buinguyen@gmail.com>
2022-04-04 17:43:29 +0700

    new Commit

---
+++ ApplicationFileConfig/ApplicationFileCongfig.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace OrderManagerment_WPF.ApplicationFileConfig
+{
+    
+    public class ApplicationFileCongfig
+    {
+        private static string Systemconfig = Directory.GetCurrentDirectory() + @"\" + "ApplicationConfig.xml";
+        #region File Initial
+        /// <summary>
+        /// Khởi tạo file mặc định
+        /// </summary>
+        /// <param name="filename"></param>
+        public static void CheckFileExit(string filename)
+        {
+            if (!File.Exists(filename))
+            {
+                File.Create(filename).Close();
+            }
+        }
+        
+        #endregion
+
+    }
+}
