72d4fc0ca5a4b3f0e841d5fc4aef735784931c15
CalisJI <quythi.buinguyen@gmail.com>
2022-04-08 10:19:13 +0700

    Brand Commit

--- OrderObject/DanhSachDonHang.cs
+++ OrderObject/DanhSachDonHang.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -17,13 +18,18 @@ namespace OrderManagerment_WPF.OrderObject
         public TrangThai Stage { get; set; }
         public Alarm Alarm { get; set; }
         public ObservableCollection<BangBaoGia> ProductDetails { get; set; }
-        
+        // Phan Thống Kê
+        public BangThongKe BangThongKe { get; set; }
     }
     public enum TrangThai 
     {
-        Dagiao,
+        [Description("Đã Giao")]
+        DaGiao,
+        [Description("PO")]
         PO,
+        [Description("Đã Báo Giá")]
         Dabaogia,
+        [Description("Chưa Báo Giá")]
         ChuaBaoGia
     }
     public enum Alarm 
